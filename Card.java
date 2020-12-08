15ec78d2b3fa45758023e8385b1f68edec1757c2
Daniel Rosales <drosales9135@gmail.com>
2020-12-08 11:17:07 -0500

    Added compare methods to card class

--- Card.java
+++ Card.java
@@ -5,6 +5,10 @@ public class Card {
 	private int rank;
 	private int suit;
 
+	public static final String[] ranks = { null, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen",
+			"King" };
+	public static final String[] suits = { "Clubs", "Diamonds", "Hearts", "Kings" };
+
 	public Card(int rank, int suit) {
 		this.rank = rank;
 		this.suit = suit;
@@ -12,9 +16,28 @@ public class Card {
 	}
 
 	public String toString() {
-		String[] ranks= {null, "Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
-		String[] suits= {null, "Diamonds","Clubs","Hearts","Spades"};
-		return ranks[this.rank]+ " of "+ suits[this.suit];
+		return ranks[this.rank] + " of " + suits[this.suit];
+	}
+
+	public boolean compareSame(Card x) {
+		return this.rank == x.rank && this.suit == x.suit;
+
+	}
+
+	public int compareCard(Card x) {
+		if (this.suit < x.suit) {
+			return -1;
+		}
+		if (this.suit > x.suit) {
+			return 1;
+		}
+		if (this.rank < x.rank) {
+			return -1;
+		}
+		if (this.suit > x.suit) {
+			return 1;
+		}
+		return 0;
 	}
 
 }
